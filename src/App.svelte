3a378e10b3e8e0140b635d201f14b42b2747bfe3
tegner <jesper.h.tegner@jppol.dk>
2021-08-10 14:13:02 +0200

    dust it

--- src/App.svelte
+++ src/App.svelte
@@ -10,15 +10,17 @@
   let tabs = [{ content: 'flurps', title: 'Step 1' }];
 </script>
 
-<main class="">
-  <h1><Icon name="ekstrabladet" /> Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.
-  </p>
-  <p>
-    And visit the <a href="https://ekstrabladetudvikling.github.io/eb-designsystem/">Designsystem documentation</a> to learn
-    how the designsystem works
-  </p>
-  <Button>Her!</Button>
-  <Accordion dataTheme="lightmode" {tabs} />
-</main>
+<div class="flex flex-justify--center">
+  <main class="grid-width--medium">
+    <h1><Icon name="ekstrabladet" /> Hello {name}!</h1>
+    <p>
+      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.
+    </p>
+    <p>
+      And visit the <a href="https://ekstrabladetudvikling.github.io/eb-designsystem/">Designsystem documentation</a> to
+      learn how the designsystem works
+    </p>
+    <Button>Her!</Button>
+    <Accordion dataTheme="lightmode" {tabs} />
+  </main>
+</div>
